355ef2f3ca1eca7b21320d302dad49e51b05632b
Pedroftoledo <ppfbtoledo@gmail.com>
2015-03-20 10:15:04 -0300

    Create Ex1_media3.py

---
+++ Ex1_media3.py
@@ -0,0 +1,9 @@
+def soma(a,b,c):
+	return ((a+b+c)/3)
+
+
+a=int(raw_input('insira 1.a nota: '))
+b=int(raw_input('insira 2.a nota: '))
+c=int(raw_input('insira 3.a nota: '))
+print soma(a,b,c)
+raw_input()
